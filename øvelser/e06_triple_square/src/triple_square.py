d11a305b4caca3c603774eeb5c4e78b668750a44
Sammy Damiri <samzan@outlook.dk>
2024-10-22 09:36:15 +0200

    e06

--- øvelser/e06_triple_square/src/triple_square.py
+++ øvelser/e06_triple_square/src/triple_square.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+def triple(a):
+    return a + a + a
+
+def square(a):
+    return a**2
 
 def main():
-    pass
+    for i in range(1,11):
+        tr = triple(i)
+        sq = square(i)
+        if(sq > tr):
+            break
+        print(f"triple({i})=={tr} square({i})=={sq}")
+
 
 if __name__ == "__main__":
     main()
