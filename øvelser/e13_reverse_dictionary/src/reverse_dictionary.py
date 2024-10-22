873570007e9d876a42b5e7c556c03c997e32117f
Sammy Damiri <samzan@outlook.dk>
2024-10-22 13:52:22 +0200

    e13

--- øvelser/e13_reverse_dictionary/src/reverse_dictionary.py
+++ øvelser/e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+    rd ={}
+    for key, vL in d.items():
+        for w in vL:
+            if w in rd:
+                rd[w] += [key]
+            else:
+                rd[w] = [key]
+                
+    return rd
 
 def main():
+    d={'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    print(reverse_dictionary(d))
     pass
 
 if __name__ == "__main__":
