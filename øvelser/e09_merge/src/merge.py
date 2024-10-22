ef141e7f60b519cba0645f2997627f9994af88c1
Sammy Damiri <samzan@outlook.dk>
2024-10-22 12:34:12 +0200

    e09

--- øvelser/e09_merge/src/merge.py
+++ øvelser/e09_merge/src/merge.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 def merge(L1, L2):
-    return []
+    MergedList = L1 + L2
+    
+    for i in range(len(MergedList)):
+        for j in range(0, len(MergedList) - i - 1):
+            if MergedList[j] > MergedList[j + 1]:
+                MergedList[j], MergedList[j + 1] = MergedList[j + 1], MergedList[j]
+        
+    return MergedList
 
 def main():
-    pass
+    l1 = [2,4,5,8,11,1,2]
+    l2 = [3,6,7,9,12]
+    print(merge(l1,l2))
 
 if __name__ == "__main__":
     main()
