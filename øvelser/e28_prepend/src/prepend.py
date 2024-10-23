95e5b273bdcb5b915449fa81399e7b0eb4d1bafd
Sammy Damiri <samzan@outlook.dk>
2024-10-23 13:08:19 +0200

    e28

--- øvelser/e28_prepend/src/prepend.py
+++ øvelser/e28_prepend/src/prepend.py
@@ -3,8 +3,16 @@
 class Prepend(object):
     # Add the methods of the class here
 
+    def __init__(self, start):
+        self.start = start
+
+    def write(self, text):
+        print(self.start + text)
+
 def main():
     pass
 
 if __name__ == "__main__":
+    p = Prepend("+++ ")
+    p.write("WazUp")
     main()
