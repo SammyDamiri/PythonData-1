ade8398bd0593e0454f1ab252603426a3e754515
Sammy Damiri <samzan@outlook.dk>
2024-10-23 09:05:29 +0200

    e16

--- øvelser/e16_transform/src/transform.py
+++ øvelser/e16_transform/src/transform.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+    L1 = s1.split()
+    L2 = s2.split()
+    zipped = zip(map(int,L1), map(int,L2))
+    return [i*j for i,j in zipped]
 
 def main():
-    pass
+    s1 = "1 5 3" 
+    s2 = "2 6 -1"
+    print(transform(s1, s2))
 
 if __name__ == "__main__":
     main()
