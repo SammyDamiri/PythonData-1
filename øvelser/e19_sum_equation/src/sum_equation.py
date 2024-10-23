e34a02cece8f3b375895ced822d81416f72904a1
Sammy Damiri <samzan@outlook.dk>
2024-10-23 10:12:19 +0200

    e19

--- øvelser/e19_sum_equation/src/sum_equation.py
+++ øvelser/e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 def sum_equation(L):
-    return ""
+    if len(L) == 0:
+        return "0 = 0"
+    left = " + ".join([str(n) for n in L])
+    right = sum(L)
+    return f"{left} = {right}"
 
 def main():
-    pass
+    print(sum_equation([1,5,7]))
 
 if __name__ == "__main__":
     main()
