d3c90126137044020ec5d25a09e13795b9fd426e
Sammy Damiri <samzan@outlook.dk>
2024-10-23 14:41:16 +0200

    e29

--- øvelser/e29_rational/src/rational.py
+++ øvelser/e29_rational/src/rational.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
+from math import gcd
 
 class Rational(object):
-    pass
+    def __init__(self, t, n):
+        self.t = t
+        self.n = n
 
+    def __str__(self):
+        cd = gcd(self.t, self.n)
+        return f"{self.t // cd}/{self.n // cd}" 
+    
+    def __add__(self, r):
+        return Rational(self.t * r.n + r.t * self.n, self.n * r.n)
+    
+    def __sub__(self, r):
+        return Rational(self.t * r.n - r.t * self.n, self.n * r.n)
+    
+    def __mul__(self, r):
+        return Rational(self.t * r.t, self.n * r.n)
+           
+    def __truediv__(self, r):
+        return Rational(self.t * r.n, self.n * r.t)
+    
+    def __eq__(self, r):
+        return self.t * r.n == self.n * r.t
+        
+    def __gt__(self, r):
+        return self.t * r.n > self.n * r.t
+
+    def __lt__(self, r):
+        return self.t * r.n < self.n * r.t
+    
 def main():
-    r1=Rational(1,4)
-    r2=Rational(2,3)
+    r1=Rational(1,8)
+    r2=Rational(1,4)
     print(r1)
     print(r2)
     print(r1*r2)
