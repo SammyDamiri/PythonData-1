cf13e5ac11d081cfab31c57e3ebe3fb341de4266
Sammy Damiri <samzan@outlook.dk>
2024-10-23 08:33:13 +0200

    e15

--- øvelser/e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ øvelser/e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    D = [(a , b) 
+         for a in range(1,7) 
+         for b in range(1,7) 
+         if a + b == 5]
+    print(*D, sep="\n")
 
 if __name__ == "__main__":
     main()
